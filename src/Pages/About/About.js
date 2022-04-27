77892aaf01e93f92881371d3dca54928e844f1bf
sayedalif <sayedalif18@gmail.com>
2022-04-27 09:15:53 +0600

    about

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div>
             <h2>About me</h2>
-            <p>Hello I'm Alif form Dinajpur, Bangladesh. I want to become a web developer. I'm practicing every free hour that i has to practice web development. It is a lifestyle so I'm also taking care of my health to perform better. Everyday i'm putting 6 hours to practice.</p>
+            <p>Hello I'm Alif form Dinajpur, Bangladesh. I want to become a web developer. I'm practicing every free hour that i has. It is a lifestyle so I'm also taking care of my health to perform better. Everyday i'm putting 6 hours for coding.</p>
         </div>
     );
 };
